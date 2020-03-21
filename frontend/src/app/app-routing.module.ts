5f2d225e5325d194c779fce781025d02f3337bf6
Christoph Otter <chipshort@tutanota.com>
2020-03-21 09:20:10 +0100

    First draft for camera integration

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {AppComponent} from './app.component';
 import {ChallengeComponent} from './challenge/challenge.component';
+import {TakephotoComponent} from './takephoto/takephoto.component';
 
 
 const routes: Routes = [
-  {path: '', component: AppComponent},
-  {path: 'challenge', component: ChallengeComponent}
+  // {path: '', component: AppComponent},
+  {path: 'challenge', component: ChallengeComponent},
+  {path: 'challenge/takephoto', component: TakephotoComponent}
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +8,7 @@ import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
 import { ChallengeComponent } from './challenge/challenge.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {WebcamModule} from 'ngx-webcam';
 
 import {DemoMaterialModule} from '../material-module';
 import { TakephotoComponent } from './takephoto/takephoto.component';
@@ -19,7 +21,9 @@ import { TakephotoComponent } from './takephoto/takephoto.component';
   ],
   imports: [
     DemoMaterialModule,
+    FormsModule,
     BrowserModule,
+    WebcamModule,
     AppRoutingModule,
     ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     BrowserAnimationsModule
