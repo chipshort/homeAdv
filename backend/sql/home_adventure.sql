b24bd25bfc108c0d911b2d21ff92edce2cf6c3f2
Felix Giese <felix.giese@icloud.com>
2020-03-21 12:10:14 +0100

    change schema again slightly

--- backend/sql/home_adventure.sql
+++ backend/sql/home_adventure.sql
@@ -1,34 +1,44 @@
+
 create table Person(
-	name varchar(50),
-	id int auto_increment primary key,
-	score integer,
-	password varchar(100)
+	id serial not null primary key,
+	name varchar(50) not null,
+	score integer not null,
+	password varchar(100) not null
 );
+
 create table Challenge(
-	title varchar(100),
-	challenge_id int auto_increment primary key,
-	topic varchar(50),
-	description text,
-	picture text,
-	begin time,
-	end time
+	id serial primary key,
+	title varchar(100) not null,
+	topic varchar(50) not null,
+	description text not null,
+	picture text not null
 );
-create table Currently_Running(
-	id int,
-	challenge_id int,
-	primary key(id, challenge_id),
-	foreign key(id) references Person(id),
-	foreign key(challenge_id) references Challenge(challenge_id),
-	status text,
-	submit varchar(50)
+
+create table ChallengeCompletion (
+	id serial not null primary key,
+	challenge_id int not null,
+	person_id int not null,
+	-- approvals int not null,
+	-- rejections int not null,
+	foreign key(challenge_id) references Challenge(id),
+	foreign key(person_id) references Person(id)
 );
-create table Verification{
-	id int,
-	challenge_id int,
-	verificator_id int,
-	primary key (id, challenge_id, verificator_id),
-	foreign key(id) references Person(id),
-	foreign key(challenge_id) references Challenge(challenge_id),
+
+-- create table Currently_Running(
+-- 	id int,
+-- 	challenge_id int,
+-- 	primary key(id, challenge_id),
+-- 	foreign key(id) references Person(id),
+-- 	foreign key(challenge_id) references Challenge(challenge_id),
+-- 	status text,
+-- 	submit varchar(50)
+-- );
+
+create table Verification (
+	completion_id int not null,
+	verificator_id int not null,
+	primary key (verificator_id, completion_id),
+	foreign key(completion_id) references ChallengeCompletion(id) on delete cascade,
 	foreign key(verificator_id) references Person(id),
-	status text
+	status int not null -- negative denied positive accepted
 );
