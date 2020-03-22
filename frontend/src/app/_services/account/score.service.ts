718bb6657dc4d143692bca0b4200602129797316
Felix Giese <felix.giese@icloud.com>
2020-03-22 14:34:09 +0100

    make account score service work

--- frontend/src/app/_services/account/score.service.ts
+++ frontend/src/app/_services/account/score.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
-import {Observable} from 'rxjs';
-import {Subject} from 'rxjs/internal/Subject';
+import { HttpClient } from '@angular/common/http';
+import { Score } from 'src/app/score';
 
 @Injectable({
   providedIn: 'root'
@@ -15,16 +13,6 @@ export class ScoreService {
   constructor(private http: HttpClient) { }
 
   getScore() {
-    const subject: Subject<number> = new Subject();
-
-    this.http.post<number>('/account/score', {}).subscribe(
-      score => {
-        subject.next(score);
-      },
-      () => {
-        subject.error(0);
-      });
-
-    return subject;
+    return this.http.get<Score>('/account/score', {});
   }
 }
